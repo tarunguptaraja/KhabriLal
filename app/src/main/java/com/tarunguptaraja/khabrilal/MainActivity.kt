cf13382d49bc2ef2b4e85e767a5e455050c605ed
tarunguptaraja <the.tarun.gupta.raja@gmail.com>
2021-04-19 20:41:21 +0530

    initial commit

---
+++ app/src/main/java/com/tarunguptaraja/khabrilal/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.tarunguptaraja.khabrilal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+
+
+class MainActivity : AppCompatActivity(),NewsItemClicked {
+
+    private lateinit var mAdapter: NewsListAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter=mAdapter
+    }
+
+    private fun fetchData() {
+        val url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3228cc7f18af45a5b073284ab8b05853"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                {
+
+                    val newsJsonArray = it.getJSONArray("articles")
+                    val newsArray = ArrayList<News>()
+                    for(i in 0 until newsJsonArray.length()) {
+                        val newsJsonObject = newsJsonArray.getJSONObject(i)
+                        val news = News(
+                                newsJsonObject.getString("title"),
+                                newsJsonObject.getString("author"),
+                                newsJsonObject.getString("url"),
+                                newsJsonObject.getString("urlToImage")
+                        )
+                        newsArray.add(news)
+                    }
+                    mAdapter.updateNews(newsArray)
+
+                },
+                { error ->
+
+                }
+        )
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    override fun onItemClicked(item: News) {
+        Toast.makeText(this,"Clicked on $item",Toast.LENGTH_LONG).show()
+    }
+}
---
+++ app/src/main/java/com/tarunguptaraja/khabrilal/News.kt
@@ -0,0 +1,8 @@
+package com.tarunguptaraja.khabrilal
+
+data class News(
+    val title: String,
+    val author: String,
+    val url: String,
+    val imageUrl: String
+)
